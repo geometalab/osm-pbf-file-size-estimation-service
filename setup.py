c37656a7bfcee70319b9360ad20aa44bfdce4983
Nicola Jordan <nic@hixi.ch>
2016-06-30 11:06:11 +0200

    only reference tested python versions

--- setup.py
+++ setup.py
@@ -89,9 +89,9 @@ setup(
         'Operating System :: OS Independent',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
