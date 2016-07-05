63faddcc4a64ae90a450c6b04521803ce5f53044
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-07-05 11:01:04 +0200

    fix typo in project setting name
    This is about character-separated values, not the Concurrent Versions System.

--- pbf_file_size_estimation/app_settings.py
+++ pbf_file_size_estimation/app_settings.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 
 PBF_FILE_SIZE_ESTIMATION_CSV_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'planet-stats.csv')
-if hasattr(settings, 'PBF_FILE_SIZE_ESTIMATION_CVS_FILE_PATH'):
-    PBF_FILE_SIZE_ESTIMATION_CSV_FILE_PATH = settings.PBF_FILE_SIZE_ESTIMATION_CVS_FILE_PATH
+if hasattr(settings, 'PBF_FILE_SIZE_ESTIMATION_CSV_FILE_PATH'):
+    PBF_FILE_SIZE_ESTIMATION_CSV_FILE_PATH = settings.PBF_FILE_SIZE_ESTIMATION_CSV_FILE_PATH
