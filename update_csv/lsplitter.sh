27644f57de95dfc52400250d7b3754e081f26a8d
Nicola Jordan <njordan.hsr@gmail.com>
2021-08-17 12:29:24 +0200

    use the faster osmiu7m library

--- update_csv/lsplitter.sh
+++ update_csv/lsplitter.sh
@@ -7,33 +7,35 @@ export tmpdir='lsplitter-tmpdir' # directory for temporary "stripes"
 export outdir='lsplitter-outdir' # directory for final tiles
 parallel_options="-v --noswap"
 
-doit1() {
+split_one_degree_pole2pole() {
 	minlat=-90
 	minlon=$1
 	maxlat=90
 	maxlon=$(($minlon+1))
 	export TIMEFORMAT=%R # http://stackoverflow.com/a/3795634
-	time osmconvert $infile -b="$minlon,$minlat,$maxlon,$maxlat" --out-pbf -o=$tmpdir/out_${minlon}.pbf --hash-memory=5000
+	osmium extract --overwrite -b "$minlon,$minlat,$maxlon,$maxlat" $infile -o $tmpdir/out_${minlon}.pbf
 }
 
-doit2() {
+split_into_one_by_one_degree() {
 	minlat=$1
 	minlon=$2
 	maxlat=$(($minlat+1))
 	maxlon=$(($minlon+1))
 	export TIMEFORMAT=%R # http://stackoverflow.com/a/3795634
-	time osmconvert $tmpdir/out_${minlon}.pbf -b="$minlon,$minlat,$maxlon,$maxlat" --out-pbf -o=$outdir/out_${minlat}_${minlon}.pbf --hash-memory=5000
+	osmium extract --overwrite -b "$minlon,$minlat,$maxlon,$maxlat" $tmpdir/out_${minlon}.pbf -o $outdir/out_${minlat}_${minlon}.pbf
 }
 
 mkdir -p $outdir
 mkdir -p $tmpdir
 
-echo "Starting phase 1..."
-
-export -f doit1 # see http://stackoverflow.com/a/26702789
-time SHELL=/bin/bash parallel ${parallel_options} doit1 ::: {-180..179} #minlon
-
-echo "Starting phase 2..."
-
-export -f doit2 # see http://stackoverflow.com/a/26702789
-time SHELL=/bin/bash parallel ${parallel_options} doit2 ::: {-90..89} ::: {-180..179} # minlat, minlon (changing this order will make things slower)
+export TIMEFORMAT=%R # http://stackoverflow.com/a/3795634
+for lon in $(seq -180 179)
+do
+	echo "slice $lon"
+	time split_one_degree_pole2pole $lon
+	for lat in $(seq -90 89)
+	do
+		echo "splits for $lat $lon"
+ 		time split_into_one_by_one_degree $lat $lon
+	done
+done
